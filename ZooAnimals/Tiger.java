81f846dbdf2a1e225be2d44d57ea0d1483be696c
Dyllan Castia <castiadyllan@gmail.com>
2026-06-24 14:05:01 -0700

    Add Tiger class with attributes and methods

---
+++ ZooAnimals/Tiger.java
@@ -0,0 +1,49 @@
+public class Tiger extends Animal implements Walk {
+
+    //Attributes
+    private int numberOfStripes;
+    private int speed;
+    private int soundLevelOfRoar;
+
+    //Constructor
+    public Tiger() {
+        super("Tiger");
+    }
+
+    //Actions
+    public void walking() {
+        System.out.println("Tiger: I am walking at speed " + speed + " mph");
+    }
+
+    @Override
+    public void eatingCompleted() {
+        System.out.println("Tiger: I have eaten meat");
+    }
+
+    //Mutators & Accessors
+    public int getNumberOfStripes() {
+        return numberOfStripes;
+    }
+
+    public void setNumberOfStripes(int numberOfStripes) {
+        this.numberOfStripes = numberOfStripes;
+    }
+
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+
+    public int getSoundLevelOfRoar() {
+        return soundLevelOfRoar;
+    }
+
+    public void setSoundLevelOfRoar(int soundLevelOfRoar) {
+        this.soundLevelOfRoar = soundLevelOfRoar;
+    }
+}
